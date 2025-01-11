08c555f0623a856297511428aab0ef02702128d2
charlieroth <charlieroth4@gmail.com>
2025-01-11 21:30:48 +0100

    read from accelerometer sensor

--- src/main.rs
+++ src/main.rs
@@ -18,16 +18,36 @@ hal::bind_interrupts!(struct Irqs {
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     info!("initializing micro-compass...");
+    // Get a handle to the peripherals
     let dp = hal::init(Default::default());
+
+    // Configure I2C using TWIM
     let config = twim::Config::default();
     let twim0 = Twim::new(dp.TWISPI0, Irqs, dp.P0_16, dp.P0_08, config);
+
+    // Initialize LSM303AGR
     let mut sensor = Lsm303agr::new_with_i2c(twim0);
+
+    // Read magnetometer ID
     match sensor.magnetometer_id().await {
         Ok(_id) => info!("magnetometer id obtained"),
         Err(_e) => info!("error getting magnetometer id"),
     }
 
+    // Initialize sensor
     sensor.init().await.unwrap();
+
+    // Configure accelerometer: High resolution mode, 50 Hz output data rate
+    sensor
+        .set_accel_mode_and_odr(
+            &mut Delay,
+            lsm303agr::AccelMode::HighResolution,
+            lsm303agr::AccelOutputDataRate::Hz50,
+        )
+        .await
+        .unwrap();
+
+    // Configure magnetometer: High resolution mode, 10 Hz output data rate
     sensor
         .set_mag_mode_and_odr(
             &mut Delay,
@@ -37,12 +57,27 @@ async fn main(_spawner: Spawner) {
         .await
         .unwrap();
 
+    // Enable continuous magnetometer mode
     let Ok(mut sensor) = sensor.into_mag_continuous().await else {
         panic!("failed to enter continuous mode");
     };
     sensor.mag_enable_low_pass_filter().await.unwrap();
 
     loop {
+        // Read accelerometer data
+        if sensor.accel_status().await.unwrap().xyz_new_data() {
+            let accel = sensor.acceleration().await.unwrap();
+            info!(
+                "Acceleration: ({}, {}, {})",
+                accel.x_mg(),
+                accel.y_mg(),
+                accel.z_mg()
+            );
+        } else {
+            warn!("No new accelerometer data available");
+        }
+
+        // Read magnetometer data
         if sensor.mag_status().await.unwrap().xyz_new_data() {
             let data = sensor.magnetic_field().await.unwrap();
             info!(
@@ -54,6 +89,8 @@ async fn main(_spawner: Spawner) {
         } else {
             warn!("No new magnetometer data available");
         }
-        Delay.delay_ms(100).await;
+
+        // Delay before next read
+        Delay.delay_ms(200).await;
     }
 }
