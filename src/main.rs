fd866a4033aee79f43d5fe545a8293b8ad378e36
charlieroth <charlieroth4@gmail.com>
2025-01-11 21:26:34 +0100

    reading from magnetometer sensor

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,59 @@
 #![no_std]
 #![no_main]
 
-use defmt::info;
+use defmt::{info, warn};
 use defmt_rtt as _;
 use embassy_executor::Spawner;
-use embassy_futures::join::join;
-use embassy_nrf::{
-    bind_interrupts,
-    gpio::{AnyPin, Input, Pin, Pull},
-    temp::Temp,
-};
-use embassy_sync::{blocking_mutex::raw::ThreadModeRawMutex, signal::Signal};
-use embassy_time::{Duration, Timer, WithTimeout};
+use embassy_nrf::{self as hal, twim::Twim};
+use embassy_time::Delay;
+use embedded_hal_async::delay::DelayNs;
+use hal::twim;
+use lsm303agr::Lsm303agr;
 use panic_probe as _;
 
-#[derive(Clone, Copy)]
-enum Button {
-    A,
-    B,
-}
-
-static SIGNAL: Signal<ThreadModeRawMutex, Button> = Signal::new();
-
-bind_interrupts!(struct Irqs {
-    TEMP => embassy_nrf::temp::InterruptHandler;
+hal::bind_interrupts!(struct Irqs {
+    TWISPI0 => twim::InterruptHandler<hal::peripherals::TWISPI0>;
 });
 
 #[embassy_executor::main]
-async fn main(spawner: Spawner) {
-    info!("Starting...");
-    let p = embassy_nrf::init(Default::default());
-    let temp = Temp::new(p.TEMP, Irqs);
-    spawner.spawn(temp_task(temp)).unwrap();
-    let button_a = button(p.P0_14.degrade(), "A", Button::A);
-    let button_b = button(p.P0_23.degrade(), "B", Button::B);
-    join(button_a, button_b).await;
-}
-
-async fn button(pin: AnyPin, id: &str, b: Button) {
-    let mut button = Input::new(pin, Pull::None);
-    loop {
-        button.wait_for_low().await;
-        info!("Button {} pressed (future)", id);
-        SIGNAL.signal(b);
-        Timer::after_millis(200).await;
-        button.wait_for_high().await;
+async fn main(_spawner: Spawner) {
+    info!("initializing micro-compass...");
+    let dp = hal::init(Default::default());
+    let config = twim::Config::default();
+    let twim0 = Twim::new(dp.TWISPI0, Irqs, dp.P0_16, dp.P0_08, config);
+    let mut sensor = Lsm303agr::new_with_i2c(twim0);
+    match sensor.magnetometer_id().await {
+        Ok(_id) => info!("magnetometer id obtained"),
+        Err(_e) => info!("error getting magnetometer id"),
     }
-}
 
-#[embassy_executor::task]
-async fn temp_task(mut temp: Temp<'static>) {
-    const INTERVAL_MS: u64 = 500;
-    let mut delay_ms = INTERVAL_MS;
+    sensor.init().await.unwrap();
+    sensor
+        .set_mag_mode_and_odr(
+            &mut Delay,
+            lsm303agr::MagMode::HighResolution,
+            lsm303agr::MagOutputDataRate::Hz10,
+        )
+        .await
+        .unwrap();
+
+    let Ok(mut sensor) = sensor.into_mag_continuous().await else {
+        panic!("failed to enter continuous mode");
+    };
+    sensor.mag_enable_low_pass_filter().await.unwrap();
+
     loop {
-        let value = temp.read().await.to_num::<u16>();
-        info!("{} C temperature received", value);
-        let delay = Duration::from_millis(delay_ms);
-        if let Some(b) = SIGNAL.wait().with_timeout(delay).await.ok() {
-            delay_ms = match b {
-                Button::A if delay_ms > INTERVAL_MS => delay_ms - INTERVAL_MS,
-                Button::A => delay_ms,
-                Button::B => delay_ms + INTERVAL_MS,
-            };
-            info!("Delay = {} ms", delay_ms);
+        if sensor.mag_status().await.unwrap().xyz_new_data() {
+            let data = sensor.magnetic_field().await.unwrap();
+            info!(
+                "magnetic field: ({}, {}, {})",
+                data.x_nt(),
+                data.y_nt(),
+                data.z_nt()
+            );
+        } else {
+            warn!("No new magnetometer data available");
         }
+        Delay.delay_ms(100).await;
     }
 }
